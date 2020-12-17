8db1d1bda2cbf48e7a0714939c2d39b93773248e
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-16 17:47:58 -0800

    adds console.log for book.legth to return the number of books in the array and adds node -r doten/config script to package.json to connect with  pgAdmin

--- data/setup.sql
+++ data/setup.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS books;
 CREATE TABLE books (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   title TEXT NOT NULL,
-  cover_image IMAGE, 
-  rating INT,
-  price INT NOT NULL,
-  in_Stock BOOLEAN NOT NULL
+  cover_image TEXT NOT NULL, 
+  rating TEXT,
+  price TEXT NOT NULL,
+  in_stock BOOLEAN NOT NULL
 );
